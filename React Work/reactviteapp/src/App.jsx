ee7ae893820bf7bf3ecbc87e1184242e36d572a2
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2024-12-13 15:40:52 +0530

    Created registration and login form

--- React Work/reactviteapp/src/App.jsx
+++ React Work/reactviteapp/src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import StudentState from "./StudentState";
-import ImageManipulation from "./ImageManipulation";
+// import React from "react";
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Registration from "./Registration";
+import MainLayout from "./MainLayout";
+// import StudentState from "./StudentState";
+// import ImageManipulation from "./ImageManipulation";
 
 function App() {
+  // const[regData,setRegData] = useState();
 
   return (
-    <div style={{display:"flex"}}> 
-      <ImageManipulation/>
+    // <div style={{display:"flex"}}> 
+      // {/* <ImageManipulation/> */}
+    
+    // <div style={{display:"flex", flexDirection:"column", marginLeft:"40px", gap:"40px"}}>
+    //   <div>{JSON.stringify(regData)}</div>
+    //   <Login loginData={regData}/>
+    //   <h3>Registration Form</h3>
+    //   <Registration registrationData={setRegData}/>
+    // </div>
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/registration" element={<Registration />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
