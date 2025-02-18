b8b9ebf16bd1979d680b2c01dc8eeea615dda3be
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-02-18 09:37:41 +0530

    Add GET and POST endpoints to httpModule for API responses

--- Backend Work/httpModule/index.js
+++ Backend Work/httpModule/index.js
@@ -2,9 +2,25 @@ const http = require('http');
 const PORT = 2527;
 const server = http.createServer((req, res) => {
     console.log("Inside Server Body");
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<div style='background-color:coral'><h1 style='color:white'>Welcome to Node Server</h1></div>\n");
-    res.end("<h2>Server has ended</h2>");
+    if(req.url == '/' && req.method == 'GET'){
+        res.setHeader('Content-Type', 'text/html');
+        // console.log(Object.keys(req));
+        //console.log(req.url + " " + req.method);
+        res.write("<div style='background-color:coral'><h1 style='color:black'>Welcome to Node Server</h1></div>\n");
+        res.end("<h2>Server has ended</h2>");
+    }
+    if(req.url == '/show' && req.method == 'GET'){
+        //res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            msg:"Successfully hit the API"
+        }))
+    }
+    if(req.url == '/show' && req.method == 'POST'){
+        //res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            msg:"Successfully hit the POST API @ /show"
+        }))
+    }
 })
 
 server.listen(PORT, () => {
